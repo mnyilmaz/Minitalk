a01c1a7d48ce616abdaea882e38714ba89b161d1
Calalari <68549106+Calalari@users.noreply.github.com>
2023-03-08 01:41:39 +0300

    Update minitalk.h

--- minitalk.h
+++ minitalk.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mervyilm <mervyilm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/20 17:59:38 by mervyilm          #+#    #+#             */
-/*   Updated: 2023/03/04 14:17:07 by mervyilm         ###   ########.fr       */
+/*   Created: 2023/01/20 18:01:32 by mervyilm          #+#    #+#             */
+/*   Updated: 2023/02/04 15:40:36 by mervyilm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,15 @@
 # include <stdarg.h>
 # include <stdlib.h>
 
-int	ft_states(char spec, va_list to_do);
-int	ft_atoi(const char *s);
-int	ft_print_char(char c);
-int	ft_print_str(char *s);
-int	ft_print_int(int num);
-
-void set_signal_action(void);
-void handle_the_server(int signal);
+int		ft_atoi(char *s);
+int		ft_states(char spec, va_list to_do);
+int		ft_print_char(char c);
+int		ft_print_str(char *s);
+int		ft_printf(char *fmt, ...);
+int		ft_print_int(int num);
+void	set_signal_action(void);
+void	handle_the_server(int signal);
+void	handle_the_client(pid_t pid, char bit);
+void reset(int *a, int *b);
 
 #endif
